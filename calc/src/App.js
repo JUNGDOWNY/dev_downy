925a34545b62ae16749a80e523e08bc6476a91fa
dev_jdowny <jdowny@enuri.com>
2024-08-05 15:55:17 +0900

    [React] calc

---
+++ calc/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+// import './App.css';
+import React from 'react';
+import Calc from './calc.js'
+
+function App() {
+  return (
+    <>
+    <Calc/>
+    </>
+  );
+}
+
+export default App;
