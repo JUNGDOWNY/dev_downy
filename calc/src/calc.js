ff9c8f5ee6aadcc58cd0426a134f8d28ff8e7c23
dev_jdowny <jdowny@enuri.com>
2024-08-08 16:12:10 +0900

    [React]calc

--- calc/src/calc.js
+++ calc/src/calc.js
@@ -2,32 +2,44 @@ import React, { useState } from 'react';
 import './calc.css';
 
 function Calc() {
-  const [monitorText, MonitorText] = useState('');
+  const [monitorText, setMonitorText] = useState('');
   const [storedValue, setStoredValue] = useState('');
   const [operator, setOperator] = useState('');
+  const [displayText, setDisplayText] = useState('');
+  const [initialInput, setInitialInput] = useState(true);
 
   const numClick = (value) => {
-    MonitorText((prevText) => prevText + value);
+    if (initialInput) {
+      setMonitorText(value);
+      setInitialInput(false);
+    } else {
+      setMonitorText((prevText) => prevText + value);
+    }
   };
 
   const handleInputChange = (event) => {
-    MonitorText(event.target.value);
+    setMonitorText(event.target.value);
   };
 
   const remove = () => {
-    MonitorText('');
+    setMonitorText('');
     setStoredValue('');
     setOperator('');
+    setDisplayText('');
+    setInitialInput(true); 
   };
 
   const backSpace = () => {
-    MonitorText(monitorText.slice(0, -1));
+    setMonitorText(monitorText.slice(0, -1));
   };
 
   const numOperator = (operatorValue) => {
+    if (monitorText === '') return; 
     setStoredValue(monitorText);
-    MonitorText('');
+    setMonitorText('');
     setOperator(operatorValue);
+    setDisplayText((prevText) => prevText + monitorText + ' ' + operatorValue + ' ');
+    setInitialInput(true); 
   };
 
   const calcResult = () => {
@@ -53,15 +65,24 @@ function Calc() {
           return;
       }
 
-      MonitorText(String(result));
+      setMonitorText(String(result));
+      setDisplayText((prevText) => prevText + monitorText + ' = ' + result); 
       setStoredValue('');
       setOperator('');
+      setInitialInput(true);
     }
+    setDisplayText('');
   };
 
   return (
     <div className='wrap'>
       <div className='calc'>
+        <input 
+          type='text' 
+          className='ipt-monitor' 
+          value={displayText}
+          readOnly 
+        />
         <input 
           type='text' 
           className='monitor' 
@@ -70,17 +91,17 @@ function Calc() {
           readOnly 
         />
         <div className='calc-button'>
-          <button onClick={() => numClick('9')}>9</button>
-          <button onClick={() => numClick('8')}>8</button>
           <button onClick={() => numClick('7')}>7</button>
+          <button onClick={() => numClick('8')}>8</button>
+          <button onClick={() => numClick('9')}>9</button>
           <button onClick={() => numOperator('+')}>➕</button>
-          <button onClick={() => numClick('6')}>6</button>
-          <button onClick={() => numClick('5')}>5</button>
           <button onClick={() => numClick('4')}>4</button>
+          <button onClick={() => numClick('5')}>5</button>
+          <button onClick={() => numClick('6')}>6</button>
           <button onClick={() => numOperator('-')}>➖</button>
-          <button onClick={() => numClick('3')}>3</button>
-          <button onClick={() => numClick('2')}>2</button>
           <button onClick={() => numClick('1')}>1</button>
+          <button onClick={() => numClick('2')}>2</button>
+          <button onClick={() => numClick('3')}>3</button>
           <button onClick={() => numOperator('*')}>✖️</button>
           <button className='btn-zero' onClick={() => numClick('0')}>0</button>
           <button onClick={() => numClick('.')}>.</button>
