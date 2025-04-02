48f454c5215f3b353432d5f86eb424e4be205f67
JUNGDOWNY <102499388+JUNGDOWNY@users.noreply.github.com>
2025-04-02 09:23:48 +0900

    Delete health directory

--- health/src/app/ingredient/page.tsx
+++
@@ -1,7 +0,0 @@
-import Image from "next/image";
-
-export default function Ingredient() {
-    return (
-        <div>성분별</div>
-    );
-}
--- health/src/app/lowest/page.tsx
+++
@@ -1,7 +0,0 @@
-import Image from "next/image";
-
-export default function Lowest() {
-    return (
-        <div>최저가보장</div>
-    );
-}
