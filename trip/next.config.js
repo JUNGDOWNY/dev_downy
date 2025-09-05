caad1bf181f40b08980f5d57861766c636f42e14
JUNGDOWNY <da98@naver.com>
2025-09-05 17:42:53 +0900

    trip

--- trip/next.config.js
+++ trip/next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  basePath: '/dev_downy/trip',
-  assetPrefix: '/dev_downy/trip',
-  images: {
-    unoptimized: true
-  }
-}
+  output: "export",
+  basePath: "/trip",
+  assetPrefix: "/trip/",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
