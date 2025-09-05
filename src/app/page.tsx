a8bbb65ded15fdd15b1f606151815d0a1cb3178c
JUNGDOWNY <da98@naver.com>
2025-09-05 10:06:20 +0900

    trip

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import Header from "../component/header/header";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>   
+     <Header />
+      <main className={styles.main}>
+        test
+      </main>
+    </div>
+  );
+}
